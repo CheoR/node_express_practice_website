4d687ea4aea2b9e23d3c6ed576fe401fef3eaea4
CheoR <cheo.roman+github@gmail.com>
2021-07-26 05:40:29 -0500

    build: serve static content.

--- server.jsx
+++ server.jsx
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = 3000;
 
+// middleware
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (req, res) => {
- res.send('Hola Cola Express Shop');
+ res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+ res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(PORT, () => {
