cc5437adfc6ade08b5409289b805fbfa14ec1b91
CheoR <cheo.roman+github@gmail.com>
2021-07-26 06:15:02 -0500

    build: configure ejs templating engine.

--- server.jsx
+++ server.jsx
@@ -5,6 +5,10 @@ const app = express();
 
 const PORT = 3000;
 
+// config template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 // middleware
 app.use(express.static(path.join(__dirname, './static')));
 
