728f194479ce805fa2109cbe9271747bd6154b7b
CheoR <cheo.roman+github@gmail.com>
2021-07-28 06:05:39 -0500

    build: add form template.

--- server.jsx
+++ server.jsx
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  console.error(err);
+  // console.error(err);
   const status = err.status || 500;
   res.locals.status = status;
   res.status(status);
