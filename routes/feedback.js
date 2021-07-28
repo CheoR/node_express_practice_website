94fa286d57fc233b8ee428d001772df3cf4b2ad0
CheoR <cheo.roman+github@gmail.com>
2021-07-28 09:31:17 -0500

    doc: create/test API endpoint.

--- routes/feedback.js
+++ routes/feedback.js
@@ -3,6 +3,29 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
+const validations = [
+  check('name')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('A name is required'),
+  check('email')
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage('A valid email address is required'),
+  check('title')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('A title is required'),
+  check('message')
+    .trim()
+    .isLength({ min: 5 })
+    .escape()
+    .withMessage('A message is required'),
+];
+
 module.exports = ( params ) => {
  const { feedbackService } = params;
  // since this is at the end of /speakers
@@ -12,9 +35,9 @@ module.exports = ( params ) => {
 
    // for after redirect on submit
    const errors = request.session.feedback ? request.session.feedback.errors : false;
-   request.session.feedback = {};
-
    const successMessage = request.session.feedback ? request.session.feedback.message : false;
+   
+   request.session.feedback = {};
 
    return res.render('layout', {
     pageTitle: 'Feedback Page',
@@ -26,58 +49,53 @@ module.exports = ( params ) => {
   } catch (err) {
    return next(err);
   }
- });
+ }); // router.get
 
  /*
   Validate form fields with check
  */
- router.post(
-  '/',
-  [
-   check('name')
-     .trim()
-     .isLength({ min: 3 })
-     .escape()
-     .withMessage('A name is required'),
-   check('email')
-     .trim()
-     .isEmail()
-     .normalizeEmail()
-     .withMessage('A valid email address is required'),
-   check('title')
-     .trim()
-     .isLength({ min: 3 })
-     .escape()
-     .withMessage('A title is required'),
-   check('message')
-     .trim()
-     .isLength({ min: 5 })
-     .escape()
-     .withMessage('A message is required'),
-  ],
-  async (req, res) => {
-   const errors = validationResult(req);
-  // console.log('from express body parser');
-  // console.log(req.body);
-  if (!errors.isEmpty()) {
+ router.post('/', validations, async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+   // console.log('from express body parser');
+   // console.log(req.body);
+   if (!errors.isEmpty()) {
+    req.session.feedback = {
+      errors: errors.array(),
+    };
+    return res.redirect('/feedback');
+   }
+
+   /*
+    Note: name, email, title, message not validated/sanitized and escaped by
+    express validator.
+   */
+   const { name, email, title, message } = req.body;
+   await feedbackService.addEntry(name, email, title, message);
    req.session.feedback = {
-     errors: errors.array(),
+     message: 'Thank you for your feedback!',
    };
+
    return res.redirect('/feedback');
+  } catch (err) {
+   return next(err);
   }
+ }); // router.post
 
-  /*
-   Note: name, email, title, message not validated/sanitized and escaped by
-   express validator.
-  */
-  const { name, email, title, message } = req.body;
-  await feedbackService.addEntry(name, email, title, message);
-  req.session.feedback = {
-    message: 'Thank you for your feedback!',
-  };
-
-  return res.redirect('/feedback');
- });
+  router.post('/api', validations, async (request, response, next) => {
+   try {
+     const errors = validationResult(request);
+     if (!errors.isEmpty()) {
+       return response.json({ errors: errors.array() });
+     }
+     const { name, email, title, message } = request.body;
+     await feedbackService.addEntry(name, email, title, message);
+     const feedback = await feedbackService.getList();
+     return response.json({ feedback });
+   } catch (err) {
+     return next(err);
+   }
+ }); // router.post
 
  return router;
 };
